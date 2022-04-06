b7b0c5802ae4c096c50ce8f2497cdf899186bac5
LucasPLA <lucas.plancon@insa-rennes.fr>
2022-04-06 17:13:01 +0200

    feat: ajout de la navbar aux pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Authent from 'pages/authent/Authent';
 import Recette from 'pages/recettes/Recette';
 import Menu from 'pages/menu/Menu';
+import Navbar from 'components/navbar/Navbar';
 
 import RequireAuth from 'utils/RequireAuth';
 import GlobalContextProvider from 'utils/GlobalContext';
 import * as AppRoutes from 'utils/routes.const';
+
+import './App.css';
 
 function App() {
 
@@ -15,14 +18,26 @@ function App() {
 		<GlobalContextProvider>
 			<BrowserRouter>
 				<Routes>
-					<Route path={AppRoutes.ROUTE_RECETTES} element={<RequireAuth><Recette /></RequireAuth>} />
-					<Route path={AppRoutes.ROUTE_MENU} element={<RequireAuth><Menu /></RequireAuth>} />
 					<Route path={AppRoutes.ROUTE_AUTHENT} element={<Authent />} />
-					<Route path={AppRoutes.BASE_ROUTE} element={<RequireAuth><Menu /></RequireAuth>} />
+					<Route path='*' element={<RequireAuth><AppWhileAuthenticated /></RequireAuth>} />
 				</Routes>
 			</BrowserRouter>
 		</GlobalContextProvider>
 	);
 }
 
+function AppWhileAuthenticated() {
+	console.log("authent");
+
+	return(
+		<div id="app">
+			<Navbar />
+			<Routes>
+				<Route path={AppRoutes.ROUTE_RECETTES} element={<RequireAuth><Recette /></RequireAuth>} />
+				<Route path={AppRoutes.ROUTE_MENU} element={<RequireAuth><Menu /></RequireAuth>} />
+				<Route path='*' element={<RequireAuth><Menu /></RequireAuth>} />
+			</Routes>
+		</div>
+	);
+}
 export default App;
