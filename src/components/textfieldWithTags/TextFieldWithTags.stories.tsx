67bb2c94648acc360da9590506fad3e10dcc8b8a
LucasPLA <zlucas.plancon@gmail.com>
2022-04-07 14:01:32 +0200

    feat: ajout d'un composant de textfield avec tag

---
+++ src/components/textfieldWithTags/TextFieldWithTags.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import TextFieldWithTags from './TextFieldWithTags';
+
+export default {
+    title: 'Composants/TextFieldWithTags',
+    component: TextFieldWithTags
+} as ComponentMeta<typeof TextFieldWithTags>;
+
+const Template: ComponentStory<typeof TextFieldWithTags> = (args) => <TextFieldWithTags {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+    label: "label",
+    buttonText: "ajouter"
+};
---
+++ src/components/textfieldWithTags/TextFieldWithTags.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+import TextField from '@mui/material/TextField';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+import './TextFieldWithTags.css';
+
+interface Props {
+    label: string;
+    buttonText: string;
+}
+
+function TextFieldWithTags({ label, buttonText } : Props) {
+    const [tagList, setTagList] = useState<string[]>([]);
+    const [tagName, setTagName] = useState<string>("");
+    
+    const handleDelete = (tag : string) => () => {
+        const tagIndex = tagList.indexOf(tag);
+        setTagList([ ...tagList.slice(0, tagIndex), ...tagList.slice(tagIndex + 1 , tagList.length)]);
+    };
+
+    const handleAdd = () => {
+        if(tagName !== "") setTagList([...tagList, tagName]);
+        setTagName("");
+    };
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTagName(event.target.value);
+    };
+
+    return(
+        <>
+            <div id="textfield-with-tags__input-row">
+                <TextField
+                    variant="outlined"
+                    onChange={handleChange}
+                    value={tagName}
+                    id="textfield-with-tags__textfield"
+                    label={label} />
+                <Button onClick={handleAdd} variant="contained">{buttonText}</Button>
+            </div>
+            <Stack direction="row" spacing={1}>
+                { tagList.map((tag, index) => 
+                    <Chip label={tag} onDelete={handleDelete(tag)} key={index}/>
+                )}
+            </Stack>
+        </>
+    );
+}
+
+export default TextFieldWithTags;
