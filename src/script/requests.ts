0b2543f7cc6b398fab69c246889abcfce2734869
LucasPLA <lucas.plancon@insa-rennes.fr>
2022-04-10 18:22:28 +0200

    refacto: ajout des types aux requêtes

--- src/script/requests.ts
+++ src/script/requests.ts
@@ -1,10 +1,11 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { Recette } from "types/recette.type";
+import { Token } from 'types/requests.type';
 
 import * as URL from 'const/routes.const';
 
 // requête de récupération du jeton d'authentification
-export const authenticate = (user : string, password : string) => {
+export const authenticate = (user : string, password : string) : Promise<AxiosResponse<Token>> => {
 	return axios.post(URL.AUTHENTICATE, {username: user, password: password});
 };
 
@@ -16,6 +17,6 @@ export const deleteRecette = (idRecette: string) => {
 	return axios.delete(URL.RECETTE + "/" + idRecette);
 };
 
-export const getRecettes = () => {
+export const getRecettes = () : Promise<AxiosResponse<Recette[]>> => {
 	return axios.get(URL.RECETTE);
 };
