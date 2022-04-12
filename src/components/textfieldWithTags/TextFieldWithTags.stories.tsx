c7ce0e5332ac6949f9fe27688c454a97b9d8a21f
LucasPLA <zlucas.plancon@gmail.com>
2022-04-12 12:03:17 +0200

    feat: ajout d'un play sur un composant storybook

--- src/components/textfieldWithTags/TextFieldWithTags.stories.tsx
+++ src/components/textfieldWithTags/TextFieldWithTags.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
+import { within, userEvent } from '@storybook/testing-library';
+
 import TextFieldWithTags from './TextFieldWithTags';
 
 export default {
@@ -16,3 +18,12 @@ Default.args = {
     buttonText: "ajouter",
     value: []
 };
+
+Default.play = async ({canvasElement}) => {
+    const canvas = within(canvasElement);
+    const textField = canvas.getByRole('textbox');
+    await userEvent.type(textField, 'un tag', {delay: 100});
+
+    const button = canvas.getByRole('button');
+    await userEvent.click(button);
+}
